e14080b06b6b511490469ca7662f5bbcc17aed9f
bygriph <140850307+bygriph@users.noreply.github.com>
2024-01-15 15:24:10 +0100

    added functions to display more weather data

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 function refreshWeather(response) {
 let temperatureElement = document.querySelector("#temperature");
 let temperature = response.data.temperature.current;
-let city = document.querySelector("#city-input");
+let cityElement = document.querySelector("#city-input");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windSpeedElement = document.querySelector("#wind-speed");
+let timeElement = document.querySelector("#time");
+let date = new Date(response.data.time * 1000);
 
-city.innerHTML = response.data.city;
+cityElement.innerHTML = response.data.city;
+timeElement.innerHTML = `${date.getDay()} ${date.getHours()}:${date.getMinutes()}`;
+descriptionElement.innerHTML = response.data.condition.description;
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 temperatureElement.innerHTML = Math.round(temperature);
  
 }
