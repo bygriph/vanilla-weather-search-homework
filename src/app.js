19c902a1c62d26dc2ebd2ed8572106997a882966
bygriph <140850307+bygriph@users.noreply.github.com>
2024-01-15 20:43:28 +0100

    injected more weather data + icon (using API)

--- src/app.js
+++ src/app.js
@@ -7,9 +7,11 @@ let humidityElement = document.querySelector("#humidity");
 let windSpeedElement = document.querySelector("#wind-speed");
 let timeElement = document.querySelector("#time");
 let date = new Date(response.data.time * 1000);
+let iconElement = document.querySelector("#icon");
 
+iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 cityElement.innerHTML = response.data.city;
-timeElement.innerHTML = `${date.getDay()} ${date.getHours()}:${date.getMinutes()}`;
+timeElement.innerHTML = formatDate(date);
 descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
@@ -17,6 +19,25 @@ temperatureElement.innerHTML = Math.round(temperature);
  
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity(city) {
   let apiKey = "d3e8e8a5a1o40279fb06ta0bd3b0fb2f";
